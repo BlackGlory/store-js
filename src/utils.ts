d3228c830e6b90c2919bf170d451a2301b7fadb4
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-03-17 18:30:26 +0000

    chore: upgrade dependencies

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
-import { HTTPOptions, HTTPOptionsTransformer } from 'extra-request'
+import { IHTTPOptions, IHTTPOptionsTransformer } from 'extra-request'
 
-export function password(password: string): HTTPOptionsTransformer {
-  return (options: HTTPOptions) => {
+export function password(password: string): IHTTPOptionsTransformer {
+  return (options: IHTTPOptions) => {
     const headers = new Headers(options.headers)
     headers.set('Authorization', `Bearer ${password}`)
     return {
